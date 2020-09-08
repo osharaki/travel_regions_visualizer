f0e43b6ea45217e288d1e2e9c34e8a8d80953729
Omar Sharaki <omarsharaki@gmail.com>
2020-09-08 19:12:46 +0200

    :sparkles: Added function to retrieve complete rows matching criteria

--- parser.js
+++ parser.js
@@ -1,9 +1,11 @@
+// @ts-check
+
 /**
- * 
- * @param {[]} rows: Array made up of rows from a parsed CSV file. Typically the data attribute of Papa.parse's response.
- * @param {int} communityLevel: Specifies the position in the community hierarchy, corresponding to the headers community_1, community_2, etc.
- * @param {int} communityId: Specifies the cluster within the community.
- * @returns {[]} An int array of the form [latitude, longitude, communityLevel]
+ * Retrieves the coordinates of locations matching certain criteria.
+ * @param {Object<string, string>[]} rows Array of objects with keys 'community_1, community_2, community_3, community_4, community_5, community_6, country_code,latitude, longitude, place_name'. Typically the data attribute of Papa.parse's response.
+ * @param {number} [communityLevel=1] Specifies the position in the community hierarchy, corresponding to the headers community_1, community_2, etc.
+ * @param {number} [communityId] Specifies the cluster within the community.
+ * @returns {number[][]} An array of the form [latitude, longitude, communityLevel].
  */
 function getCoordinates(rows, communityLevel = 1, communityId) {
     const locs = [];
@@ -20,4 +22,26 @@ function getCoordinates(rows, communityLevel = 1, communityId) {
         }
     }
     return locs;
+}
+
+/**
+ * Retrieves locations matching certain criteria
+ * @param {Object<string, string>[]} rows Array of objects with keys 'community_1, community_2, community_3, community_4, community_5, community_6, country_code,latitude, longitude, place_name'. Typically the data attribute of Papa.parse's response.
+ * @param {number} [communityLevel=1] Specifies the position in the community hierarchy, corresponding to the headers community_1, community_2, etc.
+ * @param {number} [communityId] Specifies the cluster within the community. Returns all clusters if ommited. Returns all clusters if ommited.
+ * @returns {Object<string, string>[]} The matching rows.
+ */
+function filterRows(rows, communityLevel = 1, communityId) {
+    const matchingRows = [];
+    for (const row of rows) {
+        if (communityId === undefined) { // user wants the coordinates of locations regardless of cluster
+            matchingRows.push(row);
+            continue;
+        }
+        let entryCommunityId = row[`community_${communityLevel}`];
+        if (entryCommunityId === communityId.toString()) {
+            matchingRows.push(row);
+        }
+    }
+    return matchingRows;
 }
